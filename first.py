54c85048f6c1696efedca57242122350104cab17
shreenidhi <shreenidhi.ssm@gmail.com>
2023-01-31 17:16:07 +0000

    last

--- first.py
+++ first.py
@@ -2,10 +2,12 @@ import os
 import git
 
 def first_method():
-    print("Hello")
+    print("Hello, we are learning DevOps")
 
 def sceond_method():
     print("reset")
 
 def third_method():
     print("reset")
+
+    print("Last")
